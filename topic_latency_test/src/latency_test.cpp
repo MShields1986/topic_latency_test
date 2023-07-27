c8097dd69e3bb136514dfd52181961ddbc2a9796
MShields1986 <matthew.shields.1986@gmail.com>
2023-07-27 13:23:22 +0100

    Minimal output

--- topic_latency_test/src/latency_test.cpp
+++ topic_latency_test/src/latency_test.cpp
@@ -8,7 +8,9 @@ void topicCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
   ros::Time now = ros::Time::now();
   ros::Time then = (*msg).header.stamp;
   ros::Duration dt = now - then;
-  ROS_INFO_STREAM("Packet Created: " << then << " | Packet Received: " << now << " | dt (sec): " << dt.toSec());
+
+  // ROS_INFO_STREAM("Packet Created: " << then << " | Packet Received: " << now << " | dt (sec): " << dt.toSec());
+  ROS_INFO_STREAM("Latency (sec): " << dt.toSec());
 }
 
 int main(int argc, char **argv)
