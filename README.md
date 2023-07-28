1ebedec00f1aba79cfd097fb7fd472042d864f3c
MShields1986 <matthew.shields.1986@gmail.com>
2023-07-28 16:48:50 +0100

    Logging and README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # topic_latency_test
 Package to test the latency of a ROS topic
+
+This test includes delays due to:
+- clock differences between the computers
+- network latency
+- latency between the header timestamp being populated and any other operations prior to it being sent (populating other fields etc.)
+- queuing of messages on the receiver
+- more?
--- topic_latency_test/src/latency_test.cpp
+++ topic_latency_test/src/latency_test.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+#include <fstream>
 
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
-// #include "nav_msgs/Odometry.h"
+#include "nav_msgs/Odometry.h"
+
+using namespace std;
+
+ofstream LogFile("/home/matthew/Desktop/log.txt");
 
 void topicCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 // void topicCallback(const nav_msgs::Odometry::ConstPtr& msg)
@@ -12,18 +18,23 @@ void topicCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 
   // ROS_INFO_STREAM("Packet Created: " << then << " | Packet Received: " << now << " | dt (sec): " << dt.toSec());
   ROS_INFO_STREAM("Latency (sec): " << dt.toSec());
+  LogFile << then << ", " << now << ", " << dt << endl;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "latency_tester");
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("/scan_front", 10, topicCallback);
   // ros::Subscriber sub = n.subscribe("/scan_rear", 10, topicCallback);
   // ros::Subscriber sub = n.subscribe("/odom", 10, topicCallback);
 
+  LogFile << "Packet Created, Packet Received, dt (sec)" << endl;
+
   ros::spin();
+
+  LogFile.close();
 
   return 0;
 }
